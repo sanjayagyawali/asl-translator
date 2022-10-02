cc65c0abcfbdeb5a3a5f7cd96354b537460d2953
TheB0SS37 <90638860+TheB0SS37@users.noreply.github.com>
2022-10-02 02:44:11 -0400

    Updated merge.py script to add subtitles

--- backend/scripts/merge.py
+++ backend/scripts/merge.py
@@ -7,6 +7,12 @@ if __name__ == "__main__":
    video_files = []
    for word in words:
       video_files.append(VideoFileClip(word))
-
+      # Add text to clip
+      word_txt = word.split("/")[2].split(".")[0]
+      text_clip = TextClip(word_txt, fontsize = 50, color = 'white', stroke_color = 'black', stroke_width = 2)
+      text_clip = text_clip.set_pos('bottom').set_duration(2)
+      video_files[-1] = CompositeVideoClip([video_files[-1], text_clip]) 
+      
    final = concatenate_videoclips(video_files)
-   final.write_videofile("../videos/merged-vidtest3.mp4")
+   final.write_videofile("../videos/merged.mp4")
+
