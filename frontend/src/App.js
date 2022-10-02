1c9655c232ff96ec43c5aadb7ba23eb0e9225af0
TheB0SS37 <90638860+TheB0SS37@users.noreply.github.com>
2022-10-02 06:57:08 -0400

    cors!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ const App = () => {
   async function sendToBackend()
   {
     await fetch("http://localhost:5000/translate", {
+        mode: "no-cors",
         method: 'POST',
         body: JSON.stringify({
            words: sentence,
