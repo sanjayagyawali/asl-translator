98e6b40eb89598a88624bb49e16238be3b8e1a4a
TheB0SS37 <90638860+TheB0SS37@users.noreply.github.com>
2022-10-02 03:41:41 -0400

    Removed unnecessary lines, updated directory

--- backend/scripts/merge.py
+++ backend/scripts/merge.py
@@ -1,6 +1,5 @@
-import requests
-from moviepy.editor import *
 from sys import argv
+from moviepy.editor import *
 
 if __name__ == "__main__":
    words = eval(argv[1])
@@ -10,9 +9,8 @@ if __name__ == "__main__":
       # Add text to clip
       word_txt = word.split("/")[2].split(".")[0]
       text_clip = TextClip(word_txt, fontsize = 50, color = 'white', stroke_color = 'black', stroke_width = 2)
-      text_clip = text_clip.set_pos('bottom').set_duration(2)
+      text_clip = text_clip.set_pos('bottom').set_duration(video_files[-1].duration)
       video_files[-1] = CompositeVideoClip([video_files[-1], text_clip]) 
       
    final = concatenate_videoclips(video_files)
-   final.write_videofile("../videos/merged.mp4")
-
+   final.write_videofile("../videos/final/merged.mp4")
