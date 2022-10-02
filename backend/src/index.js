3f4c82222804c1a44a6b874c6674d543ab621724
Gim949 <pjag1@umbc.edu>
2022-10-01 22:58:21 -0400

    I'll do this when I get home

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,46 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).send({
-        message: "LOOK IAN IT WORKS"
-    })
+const urlBuilder = word => `https://www.signingsavvy.com/sign/${word}`;
+
+/**
+ * 1. words param = "Tihsi sthe thing I want ot translate and such"
+ * 2. Parse words, turn into array of strings
+ * 3. Using each word, send request to that one cool website and shit
+ * 4. get those videos and download them, probably to a assets folder. 
+ * 5. Get the path to each video and create an array out of it
+ * 6. python merge.py '[\"path/to/clip1\", \"path/to/clip2\"]'
+ */
+
+app.post("/translate", async (req, res) => {
+    const { words } = req.body;
+
+    if(words && words.length) 
+    {
+        const wordList = words.split(" ");
+        const urls = wordList.map(l => urlBuilder(l));
+
+        const paths = await Promise.all(urls.map(url => downloadVideo(url)));
+        const parsedPaths = paths.map(path => `\"${path}\"`);
+        const thingToSendToPython = `'[${parsedPaths.join(",")}]'`;
+
+        const pyRes = await callPythonScript(thingToSendToPython);
+
+        res.status(200).send({
+            message: pyRes
+        });
+    }
 });
+
+async function downloadVideo(url) {
+    // Do this when I get home or sooemthing hahahahaha
+}
+
+async function callPythonScript(arg) {
+    // Yoooooooo
+}
 
 
 app.listen(process.env.PORT || 5000);
