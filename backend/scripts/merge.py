e98ce6f8180368f7612daa9fc676af936ecad369
TheB0SS37 <90638860+TheB0SS37@users.noreply.github.com>
2022-10-02 05:51:12 -0400

    fixed path bug (i hate escape characters)

--- backend/scripts/merge.py
+++ backend/scripts/merge.py
@@ -3,8 +3,11 @@ from moviepy.editor import *
 
 if __name__ == "__main__":
    words = eval(argv[1])
+
    video_files = []
    for word in words:
+      print(word)
+      print(words)
       video_files.append(VideoFileClip(word))
       # Add text to clip
       word_txt = word.split("/")[2].split(".")[0]
@@ -13,7 +16,7 @@ if __name__ == "__main__":
       video_files[-1] = CompositeVideoClip([video_files[-1], text_clip]) 
       
    final = concatenate_videoclips(video_files)
-   final.write_videofile("../videos/final/merged.mp4")
-   print("videos/final/merged.mp4")
-
+   final.write_videofile("./videos/final/merged.mp4")
    stdout.flush()
+   
+   print("videos/final/merged.mp4",end="")
